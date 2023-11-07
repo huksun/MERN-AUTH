07ea481fccc4ca49b1f1762df9f6c34e09ef3000
huksun <ngomanhhung6666@gmail.com>
2023-11-07 21:33:02 +0700

    complete sign up page ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h1 className="text-3lx text-center font-semibold">Sign up</h1>
+    </div>
+  );
 }
